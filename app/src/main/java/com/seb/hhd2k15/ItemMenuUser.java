179a16e130d41b12cf8fcb779d51b95e26552135
Nico385412 <nico.hansse@gmail.com>
2015-11-14 00:00:20 +0100

    Correction bug sur mainactivity

---
+++ app/src/main/java/com/seb/hhd2k15/ItemMenuUser.java
@@ -0,0 +1,12 @@
+package com.seb.hhd2k15;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Nicolas on 13/11/2015.
+ */
+public class ItemMenuUser {
+    private String Name;
+    private String Description;
+    private Drawable icon;
+}
