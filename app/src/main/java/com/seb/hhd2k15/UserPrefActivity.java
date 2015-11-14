b894cbe9696cc1900727dbc6552b0ce2e5c8acf0
Nico385412 <nico.hansse@gmail.com>
2015-11-14 13:34:14 +0100

    Ajout du layout des lieux

---
+++ app/src/main/java/com/seb/hhd2k15/UserPrefActivity.java
@@ -0,0 +1,16 @@
+package com.seb.hhd2k15;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Nicolas on 14/11/2015.
+ */
+public class UserPrefActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
